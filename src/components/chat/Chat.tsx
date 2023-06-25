3745fa758d2fb0d43bb68897a9f148030c6c151c
argyle-ito <ito@argyle.jp>
2023-06-25 09:21:38 +0900

    frontEdit

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Chat.scss";
+import ChatHeader from "./ChatHeader";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import GifIcon from "@mui/icons-material/Gif";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import ChatMessage from "./ChatMessage";
+const Chat = () => {
+  return (
+    <div className="chat">
+      <ChatHeader />
+      <div className="chatMessage">
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+      </div>
+      <div className="chatInput">
+        <AddCircleIcon />
+        <form>
+          <input type="text" placeholder="#Udemyメッセージを送信" />
+          <button type="submit" className="chatInputButton">
+            送信
+          </button>
+        </form>
+        <div className="chatInputIcons">
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
